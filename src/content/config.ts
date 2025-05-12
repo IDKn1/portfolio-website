16a58458756652f6d874be40dae4a90d21bb0632
idkn1 <nikolaisuvanto@gmail.com>
2025-05-12 06:21:03 -0400

    feat: added collaborators and project links to frontmatter and project page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,24 @@
 import { defineCollection, z } from "astro:content";
 
+const collaborators = z.array(
+  z.object({
+    name: z.string(),
+    url: z.string().url(),
+  })
+);
+
+const project_link = z.object({
+  name: z.string(),
+  url: z.string().url(),
+});
 const projects = defineCollection({
   schema: z.object({
     title: z.string(),
     slug: z.string().optional(),
     cover: z.string(),
     scope: z.array(z.enum(["branding", "campaign", "website"])),
+    collaborators: collaborators.optional(),
+    project_link: project_link.optional(),
   }),
 });
 
